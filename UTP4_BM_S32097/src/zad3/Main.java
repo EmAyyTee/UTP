e0354f5ff463e4370559e6616bb72323b7243c72
EmAyyTee <HanzoSwitchMail@gmail.com>
2025-04-23 15:08:48 +0200

    Finished task 3

--- UTP4_BM_S32097/src/zad3/Main.java
+++ UTP4_BM_S32097/src/zad3/Main.java
@@ -7,7 +7,34 @@
 package zad3;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+
+    Map<String, List<String>> anagramGroups = new BufferedReader(
+            new InputStreamReader(
+                    new URL("http://wiki.puzzlers.org/pub/wordlists/unixdict.txt").openStream()
+            ))
+            .lines()
+            .collect(Collectors.groupingBy(
+                    word -> {
+                      char[] chars = word.toCharArray();
+                      Arrays.sort(chars);
+                      return new String(chars);
+                    }
+            ));
+
+    anagramGroups.values()
+            .stream()
+            .filter(list -> list.size() > 1)
+            .map(group -> String.join(" ", group))
+            .sorted()
+            .forEach(System.out::println);
   }
 }
