1c231e5b0e96013ee932a478118cd46eff0dd1a3
EmAyyTee <HanzoSwitchMail@gmail.com>
2025-04-23 14:17:08 +0200

    Added constructor in task 2 with the getters

--- UTP4_BM_S32097/src/zad2/Purchase.java
+++ UTP4_BM_S32097/src/zad2/Purchase.java
@@ -8,4 +8,47 @@ package zad2;
 
 
 public class Purchase {
+
+    private String customerId;
+    private String customerName;
+    private String product;
+    private double price;
+    private double quantity;
+
+    public Purchase(String line) {
+        String[] parts = line.split(";");
+        customerId = parts[0];
+        customerName = parts[1];
+        product = parts[2];
+        price = Double.parseDouble(parts[3]);
+        quantity = Double.parseDouble(parts[4]);
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double getCost(){
+        return price * quantity;
+    }
+
+    public String toString() {
+        return String.format("%s (koszt: %.1f)", this.toString(), getCost());
+    }
 }
